0db9333787e15d746c946d76e89f29feb9cb18b3
MsTishaB <98257816+MsTishaB@users.noreply.github.com>
2022-06-02 12:56:14 +0100

    Added GitHub and Netlify links to footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
 			<Weather />
 			<footer>
 				This project was coded by Tisha Brown and is{" "}
-				<a href="/">open sourced on GitHub</a>
+				<a href="https://github.com/MsTishaB/react-weather-app">
+					open sourced on GitHub
+				</a>{" "}
+				and{" "}
+				<a href="https://playful-dusk-2dd178.netlify.app/">hosted on Netlify</a>
 			</footer>
 		</div>
 	);
