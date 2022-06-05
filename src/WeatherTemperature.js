a195763e3ad4131e50fa39d1960af3b10e985369
MsTishaB <98257816+MsTishaB@users.noreply.github.com>
2022-06-05 10:39:07 +0100

    Removed temperature unit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,52 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-	const [unit, setUnit] = useState("metric");
-
-	function convertToImperial(event) {
-		event.preventDefault();
-		setUnit("imperial");
-	}
-
-	function convertToMetric(event) {
-		event.preventDefault();
-		setUnit("metric");
-	}
-
-	if (unit === "metric") {
-		return (
-			<div className=" row WeatherTemperature">
-				<div className="col-5">
-					<h2 className="current-temp">{props.celsius}° </h2>
-				</div>
-				<div className="col-7 mt-3">
-					<span className="temp-unit">
-						C |{" "}
-						<a onClick={convertToImperial} href="/">
-							F
-						</a>
-					</span>
-				</div>
+	return (
+		<div className=" row WeatherTemperature">
+			<div className="col-5">
+				<h2 className="current-temp">{props.celsius}° </h2>
 			</div>
-		);
-	} else {
-		return (
-			<div className=" row WeatherTemperature">
-				<div className="col-5">
-					<h2 className="current-temp mt-2">
-						{Math.round(props.celsius * (9 / 5) + 32.2)}°{" "}
-					</h2>
-				</div>
-				<div className="col-7 mt-3">
-					<span className="temp-unit">
-						<a onClick={convertToMetric} href="/">
-							C
-						</a>{" "}
-						| F
-					</span>
-				</div>
+			<div className="col-7">
+				<p className="temp-unit">C</p>
 			</div>
-		);
-	}
+		</div>
+	);
 }
